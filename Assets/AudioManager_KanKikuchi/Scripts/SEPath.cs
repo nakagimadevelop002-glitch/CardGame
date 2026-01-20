60cb8c52ed28ec3d79f10a06a22e227ec7493c48
nakagimadevelop002-glitch <nakagima.develop002@gmail.com>
2026-01-20 14:48:00 +0900

    パス周りのエラー修正

--- Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -7,10 +7,10 @@ public static class SEPath{
 
 	public const string CURSOL_MOVE = "SE/CursolMove";
 	public const string DECISION    = "SE/Decision";
-	public const string JINGLE10    = "SE/Jingle/jingle10";
 	public const string SYSTEM20    = "SE/system20";
 	public const string SYSTEM23    = "SE/system23";
+    public const string JINGLE10 = "SE/system23";
 
-}
+    }
 
 }
